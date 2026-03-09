cbedb2345040e392d43155ac20537255d3ae951c
Jim <succeedhostcom@gmail.com>
2026-03-09 01:50:40 -0700

    legacy vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy' // Add this
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11'], // Covers 99% of mobile browsers
+    }),
+  ],
 })
