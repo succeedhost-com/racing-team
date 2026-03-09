26d00cd41d132bce2c73fc46e2ed833e68910bcd
Jim <succeedhostcom@gmail.com>
2026-03-09 01:58:23 -0700

    legacy vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy' // Add this
 
 export default defineConfig({
+  base: './', // Forces relative paths for all assets
   plugins: [
     vue(),
-    legacy({
-      targets: ['defaults', 'not IE 11'], // Covers 99% of mobile browsers
-    }),
+    legacy({ targets: ['defaults', 'not IE 11'] })
   ],
 })
