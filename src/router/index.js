d9b34064582d12d5029f98cb9eef7a7d89aa6b4f
Jim <succeedhostcom@gmail.com>
2026-03-09 01:10:39 -0700

    update router for not found

--- src/router/index.js
+++ src/router/index.js
@@ -4,27 +4,19 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ContactView from '../views/ContactView.vue'
 import TestView from '../views/TestView.vue'
-
-// We remove the static import of NotFound to save on initial bundle size
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/about', name: 'about', component: AboutView },
   { path: '/contact', name: 'contact', component: ContactView },
   { path: '/test', name: 'test', component: TestView },
-  
-  // Explicit path for manual testing
-  { 
-    path: '/notFound', 
-    name: 'notFoundManually', 
-    component: () => import('../views/NotFound.vue') 
-  },
-  
-  // THE CATCH-ALL ROUTE (Handles the Apache redirects)
+  { path: '/notFound', name: 'notFound', component: NotFound },
+  // THE CATCH-ALL ROUTE
   { 
     path: '/:pathMatch(.*)*', 
     name: 'NotFound', 
-    component: () => import('../views/NotFound.vue') 
+    component: NotFound
   }
 ]
 
